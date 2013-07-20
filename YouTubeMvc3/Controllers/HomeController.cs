1c829c91465dc237231b1be1eb5ba29b849fd0b9
VictorPacheco <vpacheco99@gmail.com>
2013-07-20 18:14:28 -0400

    Sample Code
    Tutorial-MVC

---
+++ YouTubeMvc3/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YouTubeMvc3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public ActionResult About(string roleName)
+        {
+            System.Web.Security.Roles.CreateRole(roleName);
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public ActionResult AddUserRole(string user)
+        {
+            System.Web.Security.Roles.AddUserToRole(user,"Members");
+            return View("About");
+        }
+
+        [Authorize(Roles = "Members")]
+        public ActionResult Members()
+        {
+            return View();
+        }
+
+        
+        
+    }
+}
---
+++ YouTubeMvc3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YouTubeMvc3.MvcApplication" Language="C#" %>
