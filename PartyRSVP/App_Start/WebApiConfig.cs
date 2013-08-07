490f8afc8260c78fa257288193fb421329b14e1e
VictorPacheco <vpacheco99@gmail.com>
2013-08-07 10:40:41 -0400

    Atom Feed

--- PartyRSVP/App_Start/WebApiConfig.cs
+++ PartyRSVP/App_Start/WebApiConfig.cs
@@ -9,11 +9,15 @@ namespace PartyRSVP
     {
         public static void Register(HttpConfiguration config)
         {
+            
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+           
         }
     }
 }
--- PartyRSVP/Controllers/RssFeedController.cs
+++ PartyRSVP/Controllers/RssFeedController.cs
@@ -10,9 +10,10 @@ namespace PartyRSVP.Controllers
 {
     public class RssFeedController : ApiController
     {
-        public Rss20FeedFormatter Get()
+      
+        public Rss20FeedFormatter GetFeed()
         {
-            var feed = new SyndicationFeed("My Blog Feed", "This is a test feed", new Uri("http://rsvpestrella.us"));
+            var feed = new SyndicationFeed("My Blog RSS Feed", "This is a test feed", new Uri("http://rsvpestrella.us"));
             feed.Authors.Add(new SyndicationPerson("someone@rsvpestrella.com"));
             feed.Categories.Add(new SyndicationCategory("How To Sample Code"));
             feed.Description = new TextSyndicationContent("This is a how to sample that demonstrates how to expose a feed using RSS with WCF");
@@ -47,6 +48,39 @@ namespace PartyRSVP.Controllers
             feed.Items = items;
 
             return new Rss20FeedFormatter(feed);
+        }
+
+        
+        public Atom10FeedFormatter GetFeed(int id)
+        {
+            SyndicationFeed feed = new SyndicationFeed("Atom Feed", "This is a test feed", new Uri("http://SomeURI"), "FeedOneID", new DateTimeOffset(DateTime.Now));
+            feed.Authors.Add(new SyndicationPerson("someone@coffee.com"));
+            feed.Categories.Add(new SyndicationCategory("How To Sample Code"));
+            feed.Description = new TextSyndicationContent("This is a sample that illistrates how to expose a feed using ATOM with WCF");
+
+            SyndicationItem item1 = new SyndicationItem(
+                "Item One",
+                "This is the content for item one",
+                new Uri("http://localhost/Content/One"),
+                "ItemOneID",
+                DateTime.Now);
+
+            SyndicationItem item2 = new SyndicationItem(
+                "Item Two",
+                "This is the content for item two",
+                new Uri("http://localhost/Content/Two"),
+                "ItemTwoID",
+                DateTime.Now);
+
+           
+            List<SyndicationItem> items = new List<SyndicationItem>();
+
+            items.Add(item1);
+            items.Add(item2);
+       
+
+            feed.Items = items;
+            return new Atom10FeedFormatter(feed);
         }
     }
 }
