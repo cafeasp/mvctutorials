3f7ea3fe995b5d4c8f19e923420e87202b4d95e8
VictorPacheco <vpacheco99@gmail.com>
2013-08-07 09:24:55 -0400

    added RSVP Tutorial

---
+++ PartyRSVP/Controllers/RSVPController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyRSVP.Models;
+
+namespace PartyRSVP.Controllers
+{
+    public class RSVPController : Controller
+    {
+        //
+        // GET: /RSVP/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(GuestRespond guestRespond)
+        {
+            if (ModelState.IsValid)
+            {
+                if (guestRespond.Attend.Value)
+                    return RedirectToAction("Thanks");
+                else return View("Sorry");
+            }
+            return View(guestRespond);
+        }
+        public ActionResult Thanks()
+        {
+            return View();
+        }
+    }
+}
---
+++ PartyRSVP/Models/GuestRespond.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyRSVP.Models
+{
+    public class GuestRespond
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+         [Required(ErrorMessage = "Please enter your email")]
+        public string Email { get; set; }
+
+         [Required(ErrorMessage = "Please enter your phone")]
+        public string Phone { get; set; }
+
+         [Required(ErrorMessage = "Please specify whether you will attend")]
+        public bool? Attend { get; set; }
+
+    }
+}
